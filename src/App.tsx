4b9153d9e7e71d62c7e9907992bec1852daea980
kamataryo <6292312+kamataryo@users.noreply.github.com>
2023-03-05 18:10:16 +0900

    Build map images

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDropzone } from 'react-dropzone'
+import { addGeojsonSourceAndLayers, addGSIPhotoImageLayer, emphasizeIsland, gpx2str } from './lib';
+// @ts-ignore
+import tj from '@mapbox/togeojson'
+import GeoJSON from 'geojson'
+import * as turf from '@turf/turf'
+import { GeoloniaMap } from '@geolonia/embed-react'
+// @ts-ignore
+import type { Map } from '@geolonia/embed'
 
 function App() {
+
+  const [geojson, setGeojson] = useState<GeoJSON.FeatureCollection<GeoJSON.LineString | GeoJSON.Point> | null>(null)
+
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    const gpxText = await gpx2str(acceptedFiles[0])
+    const gpxXml = new DOMParser().parseFromString(gpxText, 'text/xml')
+    const geojson = tj.gpx(gpxXml) as GeoJSON.FeatureCollection<GeoJSON.LineString>
+
+    const { length: totalLength } = geojson.features[0].geometry.coordinates.reduce<{ cursor: null | number[], length: number }>((prev, current) => {
+      if(prev.cursor === null) {
+
+      } else {
+        prev.length += turf.distance(prev.cursor, current, { units: 'kilometers' })
+      }
+      prev.cursor = current
+      return prev
+    } , { length: 0, cursor: null })
+
+    const startPoint = geojson.features[0].geometry.coordinates[0]
+    const endPoint = geojson.features[0].geometry.coordinates[geojson.features[0].geometry.coordinates.length - 1]
+    const enrichedGeoJSON: GeoJSON.FeatureCollection<GeoJSON.LineString | GeoJSON.Point> = {
+      type: 'FeatureCollection',
+      features: [
+        geojson.features[0],
+      ]
+    }
+    enrichedGeoJSON.features = [
+      geojson.features[0],
+      { type: 'Feature', properties: { label: '0km' }, geometry: { type: 'Point', coordinates: startPoint } },
+      { type: 'Feature', properties: { label: (Math.round(totalLength * 100) / 100) + 'km' }, geometry: { type: 'Point', coordinates: endPoint } },
+    ]
+    setGeojson(enrichedGeoJSON)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({ onDrop })
+
+  const onLoadCallback = useCallback((map: Map) => {
+    map.once('load', () => {
+      addGSIPhotoImageLayer(map)
+      addGeojsonSourceAndLayers(map, geojson)
+      emphasizeIsland(map)
+    })
+  }, [geojson])
+
   return (
+    geojson ?
+    <>
+      <script id="geojson" type="application/json">{JSON.stringify(geojson)}</script>
+      <GeoloniaMap hash={'on'} style={ {width: '100%', height: '100%'} } onLoad={onLoadCallback}></GeoloniaMap>
+    </>
+     :
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+              <p>Drop the files here ...</p> :
+              <p>Drag 'n' drop some files here, or click to select files</p>
+          }
+        </div>
       </header>
     </div>
   );
---
+++ src/lib.ts
@@ -0,0 +1,77 @@
+// @ts-ignore
+import type { Map } from '@geolonia/embed'
+
+export const gpx2str = (gpxFile: File) => {
+  const reader = new FileReader()
+  return new Promise<string>(resolve => {
+    reader.onload = (e) => {
+      resolve(e.target?.result as string)
+    }
+    reader.readAsText(gpxFile)
+  })
+}
+
+export const addGeojsonSourceAndLayers = (map: Map, geojson: any) => {
+  map.addSource('track', {
+    type: 'geojson',
+    data: geojson,
+  })
+  map.addLayer({
+    id: 'track',
+    type: 'line',
+    filter: ['==', '$type', 'LineString'],
+    source: 'track',
+    paint: {
+      'line-width': 2,
+      'line-color': 'red',
+    }
+  }, 'place-island-name')
+  map.addLayer({
+    id: 'track-end',
+    type: 'circle',
+    filter: ['==', '$type', 'Point'],
+    source: 'track',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': 'red',
+    },
+  }, 'place-island-name')
+  map.addLayer({
+    id: 'track-end-label',
+    type: 'symbol',
+    source: 'track',
+    filter: ['==', '$type', 'Point'],
+    layout: {
+      'text-field': '{label}',
+      'text-font': ["Noto Sans Regular"],
+      'text-anchor': 'top',
+      'text-offset': [0, 0.3],
+    },
+    paint: {
+      'text-halo-color': 'white',
+      'text-halo-width': 2,
+      'text-halo-blur': 0.5,
+    }
+  }, 'place-island-name')
+}
+
+export const addGSIPhotoImageLayer = (map: Map) => {
+  map.addSource('gsi-photo', {
+    type: 'raster',
+    tiles: ['https://maps.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg'],
+    tileSize: 256,
+    attribution: '国土地理院 全国最新写真',
+  })
+  map.addLayer({
+    'id': 'gsi-photo',
+    'type': 'raster',
+    'source': 'gsi-photo',
+    'minzoom': 0,
+    'maxzoom': 22,
+    }, 'oc-waterway-river-ja')
+}
+
+export const emphasizeIsland = (map: Map) => {
+  map.setLayoutProperty('place-island-name', 'text-size', 14)
+  map.setPaintProperty('place-island-name', 'text-color', 'black')
+}
